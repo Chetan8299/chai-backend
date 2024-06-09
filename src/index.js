3bd37012aa3b46e8dcbd6142a69c8cf58f425880
Chetan8299 <csingh8299@gmail.com>
2024-06-09 10:41:45 +0530

    Add logout and auth midlleware

--- src/index.js
+++ src/index.js
@@ -1,46 +1,30 @@
 // require('dotenv').config({path: './env'})
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import {app} from './app.js'
+import { app } from "./app.js";
 
-dotenv.config({ path: './.env' })
+dotenv.config({ path: "./.env" });
 
 connectDB()
-.then(() => {
-    app.on('error', (err) => {
-        console.log("Error: ", err)
-        throw err;
-    })
-    app.listen(process.env.PORT || 8000, () =>  {
-        console.log(`App listening on port ${process.env.PORT}`)
-    } )
-})
-.catch((err) => {
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error: ", err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`App listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("MONGODB connection failed: " + err);
-})
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// First approach to connect to the database    
+// First approach to connect to the database
 // import express from "express";
 // const app = express();
 // ( async () => {
 //     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);        
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
 //         app.on("error", (error) => {
 //             console.log("Error: ",error);
 //             throw error;
